5f990639c8a96f3ca14200e46bdde117ccd7f5e3
sulta <sultandjavgashev@gmail.com>
2023-07-27 00:25:12 +0500

    add new classes DB, Author, News.

---
+++ module-repository/src/main/java/com/mjc/school/repository/Author.java
@@ -0,0 +1,22 @@
+package com.mjc.school.repository;
+
+public class Author {
+    private Long id;
+    private String Name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
---
+++ module-repository/src/main/java/com/mjc/school/repository/News.java
@@ -0,0 +1,60 @@
+package com.mjc.school.repository;
+
+import java.time.LocalDateTime;
+
+public class News {
+    private int id;
+    private String title;
+    private String content;
+    private LocalDateTime createDate;
+    private LocalDateTime lastUpdateDate;
+    private Long authorID;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public LocalDateTime getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(LocalDateTime lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
+    public Long getAuthorID() {
+        return authorID;
+    }
+
+    public void setAuthorID(Long authorID) {
+        this.authorID = authorID;
+    }
+}
